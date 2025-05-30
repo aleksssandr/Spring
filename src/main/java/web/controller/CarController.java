d9521eb1467e9c1837694a8fd78823f325397e3d
alekssboy <aliaksandrbaiko1@gmail.com>
2025-05-30 12:12:44 +0300

    init

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,23 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+
+@Controller
+public class CarController {
+
+    private CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping(value = "/cars")
+    public String showCars(Model model, @RequestParam(value = "count", required = false, defaultValue = "5") Integer count) {
+        model.addAttribute("cars", carService.getCarsByCount(count));
+        return "cars";
+    }
+}
